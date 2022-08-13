37e52790c2d52966e93f4da2b7501e0409411209
Park Min Hyeok <opensource@pmh.codes>
2022-08-13 22:58:58 +0900

    feat: add anon command

--- src/commands/anon.ts
+++ src/commands/anon.ts
@@ -1,19 +1,18 @@
 import Command from '../interfaces/Command'
 import { I, D } from '../aliases/discord.js.js'
-import { WebhookClient } from 'discord.js'
+import { TextChannel } from 'discord.js'
 
 /** 핑 명령어 */
 export default class PingCommand implements Command {
   /** 실행되는 부분입니다. */
   async run (interaction: I) {
-    const webhook = new WebhookClient({
-      id: process.env.ANON_WEBHOOK_ID!,
-      token: process.env.ANON_WEBHOOK_TOKEN!
-    })
+    const channel = interaction.channel as TextChannel
+    const webhook = await channel.createWebhook('???')
     const message = interaction.options.getString('message', true)
 
     await interaction.editReply('완료!')
     await webhook.send(message)
+    await webhook.delete()
   }
 
   /** 해당 명령어의 대한 설정입니다. */
